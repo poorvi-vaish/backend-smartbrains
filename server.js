a672bdefbf58335cf3c57356f3b0f7df3169561e
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-06-11 20:36:00 +0530

    Added dotenv

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.post("/imageurl", (req, res) => {
   handleAPIcall(req, res);
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT ||  3000, () => {
   console.log("hey its working!");
 });
